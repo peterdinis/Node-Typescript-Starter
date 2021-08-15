3eb9431726b775f0ffa8b602758122c9317aba3a
Peter Dinis <peterdinis0@pobox.sk>
2021-08-15 18:10:06 +0200

    Start to updating starter template

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,8 @@ import express, {Application, Request, Response, NextFunction} from 'express';
 
 const app: Application = express();
 
-const add = (a: number, b: number): number => a + b;
-
 app.get('/', (req: Request, res: Response, next: NextFunction) => {
-    res.send('Test');
-    console.log(add(4, 5));
+    res.send('Ping');
 });
 
 app.listen(4000, () => console.log('Server running'));
---
+++ src/controllers/starterController.ts
@@ -0,0 +1,5 @@
+import { Response, Request} from "express";
+
+export const starterFn = (req: Request, res: Response) => {
+    res.send("WORK");
+}
