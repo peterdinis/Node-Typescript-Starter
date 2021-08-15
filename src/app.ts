1f8aa7d3b02970d5f2363e575bc028cb986a5327
Peter Dinis <peterdinis0@pobox.sk>
2021-08-15 18:37:40 +0200

    Edit views and update app

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,15 @@
 import express, {Application, Request, Response, NextFunction} from 'express';
+import path from "path";
+import compression from "compression";
+import bodyParser from "body-parser";
 
 const app: Application = express();
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(compression());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req: Request, res: Response, next: NextFunction) => {
     res.send('Ping');
