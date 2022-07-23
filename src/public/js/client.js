c83f39100903d617e63e55fea86321fe8911f928
Peterdinis <peterdinis0@pobox.sk>
2022-07-23 22:56:07 +0200

    Small Updates

--- src/public/js/client.js
+++ src/public/js/client.js
@@ -1,1 +1,6 @@
-console.log("WORK");
+const btn = document.querySelector("button.mobile-menu-button");
+const menu = document.querySelector(".mobile-menu");
+
+btn.addEventListener("click", () => {
+    menu.classList.toggle("hidden");
+});
