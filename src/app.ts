344224184e4619da4ad4d44367f4e50044305a5b
Peterdinis <peterdinis0@pobox.sk>
2022-09-05 20:17:02 +0200

    Small updates in routes and controller

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, {Application} from 'express';
 import path from "path";
 import compression from "compression";
+import startRoutes from "./routes/staterRoutes";
 
 const app: Application = express();
 
@@ -8,6 +9,7 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(compression());
 app.use(express.json());
+app.use(startRoutes);
 app.use(
     express.static(path.join(__dirname, "public"))
 );
--- src/controllers/starterController.ts
+++ src/controllers/starterController.ts
@@ -1,5 +1,5 @@
 import { Response, Request} from "express";
 
 export const starterFn = (req: Request, res: Response) => {
-    res.send("WORK");
+    res.render("home");
 }
