dba2973b496924a1eccce6054640fdabaebb7665
Peterdinis <peterdinis0@pobox.sk>
2022-09-05 20:18:13 +0200

    FOO

--- ReadME.md
+++ ReadME.md
@@ -5,7 +5,6 @@ Simple example template for node-typescript apps
 Stack:
 - package manager npm
 - testing: jest + jest-extended
-- views: ejs
 - Add public css and js files
 - add example for controllers and routing
 - Add .env file
--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,11 @@
 import express, {Application} from 'express';
-import path from "path";
 import compression from "compression";
 import startRoutes from "./routes/staterRoutes";
 
 const app: Application = express();
 
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
 app.use(compression());
 app.use(express.json());
 app.use(startRoutes);
-app.use(
-    express.static(path.join(__dirname, "public"))
-);
 
 export default app;
